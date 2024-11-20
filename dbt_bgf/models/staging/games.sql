18e622e84fed4c4c97c7c808073db5f54babcac7
Erman Torun <etorun@gmail.com>
2024-11-20 12:48:44 +0000

    dbt folder structure

--- dbt_bgf/models/staging/games.sql
+++ dbt_bgf/models/staging/games.sql
@@ -8,8 +8,10 @@ SELECT
   , year                                      AS year_published
   , min_players                               AS min_players
   , max_players                               AS max_players
-  , playing_time                              AS game_time
-  , avg_rating                                AS game_rating
+  , playing_time                              AS game_duration
+  , min_playing_time                          AS game_min_duration
+  , max_playing_time                          AS game_max_duration
+  , min_age                                   AS game_rating
   , categories                                AS game_category
   , mechanics                                 AS game_mechanics
   , themes                                    AS gmae_themes
@@ -17,4 +19,5 @@ SELECT
   , NumWish                                   AS nr_wished_users
   , NumUserRatings                            AS nr_rated_users
 
--- FROM {{ source('XXX') }}
+
+{{ source('bgf_project_silver', 'boardgamefinder') }}
