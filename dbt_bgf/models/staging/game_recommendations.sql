633376e873942b50eafdc93f6fa2f815030be637
ardademirci <ademircies@gmail.com>
2024-11-20 13:58:38 +0000

    WIP: Save progress of dbt

---
+++ dbt_bgf/models/staging/game_recommendations.sql
@@ -0,0 +1,13 @@
+
+{{config(materialized='table')}}
+
+SELECT
+    id AS game_id,
+    ComMinPlaytime AS community_min_playtime,
+    ComMaxPlaytime AS community_max_playtime,
+    CAST(ComAgeRec AS INT64) AS community_age_reco,
+    MfgAgeRec AS manufacturer_age_reco,
+    MfgPlaytime AS manufacturer_reco_playtime
+
+{{ source('bgf_project_silver', 'boardgamefinder') }}
+
